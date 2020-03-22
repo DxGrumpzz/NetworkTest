cf2e7b131a3d8c1fcf6b1820dd8535ef3be1eefe
Josef Lintz <yosi1.lints@gmail.com>
2020-03-22 14:09:40 +0200

    * Moved TestClass to seperate file

---
+++ Core/Models/TestClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Core
+{
+    using System.Collections.Generic;
+
+    public class TestClass
+    {
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public bool Bool { get; set; }
+
+        public IEnumerable<int> Enumerable { get; set; }
+    }
+}
