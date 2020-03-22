c30cd6852d12daeae458595600e05ee1990e99df
Josef Lintz <yosi1.lints@gmail.com>
2020-03-22 16:02:22 +0200

    * Moved Controller to speperate file

---
+++ Server/Controllers/Controller.cs
@@ -0,0 +1,31 @@
+﻿namespace Server
+{
+    using Core;
+
+    using System;
+
+    public class Controller
+    {
+
+        public ActionResult Action()
+        {
+            Console.WriteLine($"Client called {nameof(Controller)}/{nameof(Action2)}");
+
+            return new ActionResult(ActionStatus.Success);
+        }
+
+        public ActionResult Action2(TestClass s)
+        {
+            Console.WriteLine($"Client called {nameof(Controller)}/{nameof(Action2)}");
+
+            Console.WriteLine($"Recevied {s.Text}");
+
+            return new ActionResult()
+            {
+                Data = "mega succ",
+                Result = ActionStatus.Success,
+            };
+        }
+
+    }
+};
