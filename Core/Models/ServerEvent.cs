4a769170d3a695c0276d8b4e2bb261df164be471
Josef Lintz <yosi1.lints@gmail.com>
2020-03-22 14:07:29 +0200

    + Added ServerEvent class

---
+++ Core/Models/ServerEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace Core
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServerEvent
+    {
+        public string EventName { get; set; }
+
+        public byte[] Data { get; set; }
+        
+        public string DataTypename { get; set; }
+
+        public bool EventHasArgs => Data != null;
+    };
+};
