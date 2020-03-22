7ca8a2e34e246442a1dfb453d06c7fc25e50756d
Josef Lintz <yosi1.lints@gmail.com>
2020-03-22 18:00:15 +0200

    * Moved ControllerActionInfo to seperate file

---
+++ Server/Models/ControllerActionInfo.cs
@@ -0,0 +1,45 @@
+﻿namespace Server
+{
+    using System.Linq;
+    using System.Reflection;
+
+    public class ControllerActionInfo
+    {
+        private ControllerBase _controller;
+        
+        public MethodInfo MethodInfo { get; }
+
+        public bool ActionHasParameters => MethodInfo.GetParameters().Count() > 0;
+
+        public string ActionName => MethodInfo.Name;
+
+        public ParameterInfo[] GetParameters => MethodInfo.GetParameters();
+
+
+        public ControllerActionInfo(MethodInfo methodInfo, ControllerBase controller)
+        {
+            MethodInfo = methodInfo;
+            _controller = controller;
+        }
+
+
+
+        public ActionResult Invoke()
+        {
+            return (ActionResult)MethodInfo.Invoke(_controller, null);
+        }
+
+        public ActionResult Invoke<T>(T arg)
+        {
+            return (ActionResult)MethodInfo.Invoke(_controller, new[] { (object)arg });
+        }
+
+        public ActionResult Invoke(params object[] args)
+        {
+            return (ActionResult)MethodInfo.Invoke(_controller, args);
+
+        }
+
+    }
+};
+
--- Server/TestTcpServer.cs
+++ Server/TestTcpServer.cs
@@ -4,10 +4,8 @@
 
     using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Net;
     using System.Net.Sockets;
-    using System.Reflection;
     using System.Threading.Tasks;
 
 
@@ -230,45 +228,5 @@
         }
 
     };
-
-
-    public class ControllerActionInfo
-    {
-        private ControllerBase _controller;
-        
-        public MethodInfo MethodInfo { get; }
-
-        public bool ActionHasParameters => MethodInfo.GetParameters().Count() > 0;
-
-        public string ActionName => MethodInfo.Name;
-
-        public ParameterInfo[] GetParameters => MethodInfo.GetParameters();
-
-
-        public ControllerActionInfo(MethodInfo methodInfo, ControllerBase controller)
-        {
-            MethodInfo = methodInfo;
-            _controller = controller;
-        }
-
-
-
-        public ActionResult Invoke()
-        {
-            return (ActionResult)MethodInfo.Invoke(_controller, null);
-        }
-
-        public ActionResult Invoke<T>(T arg)
-        {
-            return (ActionResult)MethodInfo.Invoke(_controller, new[] { (object)arg });
-        }
-
-        public ActionResult Invoke(params object[] args)
-        {
-            return (ActionResult)MethodInfo.Invoke(_controller, args);
-
-        }
-
-    }
 };
 
